bf43bddb4748427db124549720b91f675734ed6d
Philipp Knechtges <philipp.knechtges@dlr.de>
2020-06-03 10:48:33 +0200

    add more tests for non-blocking

--- tests/test_nonblocking.py
+++ tests/test_nonblocking.py
@@ -15,3 +15,21 @@ class TestNonBlocking(unittest.TestCase):
         res3.sum().backward()
         self.assertTrue((tmp.grad == ((comm.rank + 1 )%comm.size) * torch.ones_like(tmp)).all())
 
+    def test_simple_isendrecv(self):
+        tmp = torch.rand(10000000, dtype=torch.double).requires_grad_()
+        req = comm.Isend(tmp,(comm.rank+1)%comm.size,0)
+        res = comm.Recv(torchmpi.JoinDummies(torch.empty_like(tmp),[req[0]]),(comm.rank+comm.size-1)%comm.size,0)
+        res2 = comm.Wait(torchmpi.JoinDummiesHandle(req,[res]))
+        res3 = torchmpi.JoinDummies(res,[res2]) * comm.rank
+        res3.sum().backward()
+        self.assertTrue((tmp.grad == ((comm.rank + 1 )%comm.size) * torch.ones_like(tmp)).all())
+
+    def test_simple_irecvsend(self):
+        tmp = torch.rand(10000000, dtype=torch.double).requires_grad_()
+        req = comm.Irecv(torchmpi.JoinDummies(torch.empty_like(tmp),[tmp]),(comm.rank+comm.size-1)%comm.size,0)
+        res = comm.Send(tmp,(comm.rank+1)%comm.size,0)
+        res2 = comm.Wait(torchmpi.JoinDummiesHandle(req,[res]))
+        res3 = res2 * comm.rank
+        res3.sum().backward()
+        self.assertTrue((tmp.grad == ((comm.rank + 1 )%comm.size) * torch.ones_like(tmp)).all())
+
